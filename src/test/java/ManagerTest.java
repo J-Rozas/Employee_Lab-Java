a850ebb8fb32e7329fdcffd3c043ff58ad31dcef
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-15 17:27:42 +0100

    Add tests for Manager class

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class ManagerTest {
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ManagerTest {
+    private Manager manager;
+
+    @Before
+    public void before() {
+        manager = new Manager("John", "1234abc56", 5000000, "Sales");
+    }
+
+    @Test
+    public void shouldHaveName() {
+        assertEquals("John", manager.getName());
+    }
+
+    @Test
+    public void shouldHaveNINo() {
+        assertEquals("1234abc56", manager.getNINo());
+    }
+
+    @Test
+    public void shouldHaveSalary() {
+        assertEquals(50000.00, manager.getSalary(), 0.0);
+    }
+
+    @Test
+    public void shouldHaveDepartment() {
+        assertEquals("Sales", manager.getDepartmentName());
+    }
+
+    @Test
+    public void shouldBeAbleToGetSalaryRaise() {
+        manager.raiseSalary(5000.24);
+        assertEquals(55000.24, manager.getSalary(), 0.0);
+    }
+
+    @Test
+    public void shouldBeAbleToGetBonus() {
+        assertEquals(500.00, manager.payBonus());
+    }
 }
