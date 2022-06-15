95d94779667d1cfc7768624aa6603620851f954d
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-06-15 18:30:07 +0100

    Add tests for Developer class

--- src/test/java/DeveloperTest.java
+++ src/test/java/DeveloperTest.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class DeveloperTest {
+import management.Manager;
+import org.junit.Before;
+import org.junit.Test;
+import techStaff.Developer;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeveloperTest {
+    private Developer developer;
+
+    @Before
+    public void before() {
+        developer = new Developer("Dave", "193jfn83", 8000000);
+    }
+
+    @Test
+    public void shouldHaveName() {
+        assertEquals("Dave", developer.getName());
+    }
+
+    @Test
+    public void shouldHaveNINo() {
+        assertEquals("193jfn83", developer.getNINo());
+    }
+
+    @Test
+    public void shouldHaveSalary() {
+        assertEquals(80000.00, developer.getSalary(), 0.0);
+    }
+
+    @Test
+    public void shouldBeAbleToGetSalaryRaise() {
+        developer.raiseSalary(1.1);
+        assertEquals(80001.1, developer.getSalary(), 0.0);
+    }
+
+    @Test
+    public void shouldBeAbleToGetBonus() {
+        assertEquals(800.00, developer.payBonus(), 0.0);
+    }
 }
